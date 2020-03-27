d8557d596f1841945430dd408e7d3e60d0c249d6
Joshua Marino <joshuamarino@joshuas-mbp.home>
2020-03-27 09:56:12 -0400

    mar24

---
+++ Lab/Lab07_Marino/lab07_v1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*
+*   Lab 07: Hangman
+*   
+*
+*
+*/
+
+// Functions
+void print_array(int [], int);
+
+int main () {
+  
+    
+    return 0;
+}
+
+
+void print_array(int array[], int SIZE){
+    for(int i = 0; i < SIZE; i++){
+           printf("%d ", array[i]);
+        }
+    printf("\n");    
+    return;
+}
