aa54a095bd568627727e82143be3549394c8d82f
Joshua Marino <josh.marino@sbcglobal.net>
2020-05-01 10:41:06 -0400

    may01

---
+++ Lab/Lab10_Marino/lab10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+* Currently CEG2170 Template
+* See Makefile for what is happening in background for "make run"
+*/
+
+int main () {
+    printf("Hello, World!\n");
+    return 0;
+}
